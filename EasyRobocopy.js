5f541320db71c84d3f28ab5995afa14c4b733892
ashb2612 <ashb2612@gmail.com>
2024-02-12 22:45:43 +0000

    UI options now populated from stored object properties

--- EasyRobocopy.js
+++ EasyRobocopy.js
@@ -5,15 +5,21 @@ class Option {
   }
 }
 
-function init() {
-  var options = {
-    s: new Option(true, 1),
-    e: new Option()
+// store all options
+var options = {}
+
+function updateOptionsGui() {
+  // updates all checkboxes using the stored options
+  for (o in options) {
+    if (options[o].isEnabled === true) {
+      // is true
+      document.getElementById(o).checked = true;
+    }
+    else {
+      // is false or undefined
+      document.getElementById(o).checked = false;
+    }
   }
-
-  console.log(options.s.value)
-
-  computeCommand()
 }
 
 function computeCommand() {
@@ -29,6 +35,19 @@ function updateDisplayedCommand(cmd) {
 function copyToClipboard() {
   var cmd = document.getElementById("computedCommand");
   navigator.clipboard.writeText(cmd.innerHTML);
+}
+
+
+function init() {
+  // initialise everything
+
+  // sample options for testing
+  options.s = new Option(false, undefined),
+  options.e = new Option()
+  options.lev = new Option(true, 5)
+
+  updateOptionsGui()
+  computeCommand()
 }
 
 // event listeners
